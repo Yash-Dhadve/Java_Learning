d3e6942aa9dc6b518dc9b306aa53a030127ab07c
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-03-02 15:06:13 +0530

    feat: create Animal.class, Dog.class, Main.class and Main.java

---
+++ Day-6/Practice-Set/Main.java
@@ -0,0 +1,34 @@
+/*
+Create class Animal with method makeSound()
+Create class Dog extending Animal
+Override makeSound()
+ */
+class Animal{
+
+    String name;
+    Animal(String name){
+        this.name = name;
+    }
+
+    void makeSound(){
+        System.out.println("Animal makes sound...");
+    }
+}
+
+class Dog extends Animal{
+
+    Dog(String name){
+        super(name);
+    }
+    void makeSound(){
+        System.out.println("Bow Bow...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Tommy");
+        System.out.println("Brand: " + dog.name);
+        dog.makeSound();
+    }
+}
