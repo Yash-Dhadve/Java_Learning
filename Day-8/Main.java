813c987fd669dbf5d0559a28d5867022a376c1cc
Yash Dhadve <lightningmonster.1548@gmail.com>
2026-03-04 13:35:28 +0530

    feat: create Circle.class, Main.class, Main.java and Shape.class

---
+++ Day-8/Main.java
@@ -0,0 +1,23 @@
+abstract class Shape {
+    abstract double area();   // no body
+}
+
+class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        Circle c1 = new Circle(3);
+
+        System.out.println("Area of circle: "+c1.area());
+    }
+}
